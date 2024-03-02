f3ed2d8f53226aae9c016418da4b54c0862c27b6
ZHtttttttt <1099480504@qq.com>
2024-03-02 15:54:26 +0800

    feat: update build outputPath

--- config/config.ts
+++ config/config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
   antd: {},
   routes,
   fastRefresh: {},
+  outputPath: 'docs',
+  chainWebpack(memo, { env, webpack }) {
+    // 删除 umi 内置插件
+    memo.plugins.delete('copy');
+  },
   theme: {
     'primary-color': '#1DA57A',
     '@border-radius-base': '6px',
